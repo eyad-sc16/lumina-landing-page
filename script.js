e4bdfbb6b3280684c396fba7268ec1be0413807a
Eyad nasser <eyadnasser499@gmail.com>
2026-08-21 15:28:45 +0300

    implementing smooth scroll to the whole nav links using the delegation pattern

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 'use strict'
 
 ////////////////DOM Elements///////////////////
-
+const learnMoreBtn = document.querySelector('.learn-more-btn')
+const brandHeaderLink = document.getElementById('brand')
 const siteNav = document.getElementById('site-nav');
-
-
-
-
+const headerActionEle = document.querySelector('.header-actions')
 
 
 ////////////////////// functionality ///////////////////
 
+// handling scroll to learn more btn 
+learnMoreBtn.addEventListener('click', function (e) {
+  e.preventDefault()
+  document.querySelector(e.target.getAttribute('href')).scrollIntoView({ behavior: 'smooth' })
+});
+
+// handling brand scroll 
+brandHeaderLink.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  if (e.target.classList.contains('inner-brand-element'))
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' })
+});
+
+// handling the smooth navigaton of the nav links
+siteNav.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  const lifeCollectionArr = [...this.children]
+
+  // matching strategy 
+  if (lifeCollectionArr.some(ele => ele === e.target)) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  }
+});
+
+// handling funtionality of header actons part
+headerActionEle.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  if ([...this.children].some(ele => ele === e.target)) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  }
+});
 
